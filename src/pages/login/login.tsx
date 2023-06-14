a98432c0b5e690b1b6be3d950bc2f1b0c5c69542
tang <15977536060@163.com>
2023-06-14 14:46:21 +0800

    refactor: improve the robustness of login page

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,60 +1,79 @@
 import { Form, Button, Input } from "antd-mobile";
 import { useState } from "react";
+import { createUser } from "../../api/user";
 
 import "./login.css";
-import { createUser } from "../../api/user";
 
 export default function Login() {
   const [form] = Form.useForm<{ Email: string; VerificationCode: string }>();
-  const onSubmit = () => {
+  const onFinish = () => {
     const values = form.getFieldsValue();
     createUser(values).catch((err) => {
       console.error(err);
     });
   };
 
-  const validateMessages = {
-    // eslint-disable-next-line no-template-curly-in-string
-    required: "${name} is required ",
-  };
-
+  // 1. Validate the Email field
+  // 2. Create account
+  // 3. Countdown for disabled the "Send Code" button
   const [countdown, setCountdown] = useState(0);
-  const handleSendCode = () => {
-    const values = form.getFieldsValue();
-    values.VerificationCode = "";
-    createUser(values).catch((err) => {
-      console.error(err);
-    });
-    if (countdown === 0) {
-      setCountdown(60);
-      const timer = setInterval(() => {
-        setCountdown((prevCountdown) => {
-          if (prevCountdown === 1) clearInterval(timer);
-          return prevCountdown - 1;
-        });
-      }, 1000);
+  const handleSendCode = async () => {
+    try {
+      await form.validateFields(["Email"]);
+
+      const values = form.getFieldsValue();
+      values.VerificationCode = "";
+      await createUser(values);
+
+      if (countdown === 0) {
+        setCountdown(60);
+        const timer = setInterval(() => {
+          setCountdown((prevCountdown) => {
+            if (prevCountdown === 1) clearInterval(timer);
+            return prevCountdown - 1;
+          });
+        }, 1000);
+      }
+    } catch (errors) {
+      console.log(errors);
     }
   };
 
   return (
     <Form
       form={form}
-      validateMessages={validateMessages}
       layout="horizontal"
       style={{ "--prefix-width": "3em" }}
+      onFinish={onFinish}
       footer={
-        <Button onClick={onSubmit} block type="submit" color="primary">
+        <Button block type="submit" color="primary">
           Login
         </Button>
       }
     >
-      <Form.Item name="Email" label="Email" rules={[{ required: true }]}>
+      <Form.Item
+        name="Email"
+        label="Email"
+        rules={[
+          {
+            required: true,
+            whitespace: false,
+            message: "Input your email adress",
+          },
+        ]}
+      >
         <Input placeholder="Input your email adress" />
       </Form.Item>
       <Form.Item
         name="VerificationCode"
         label="Code"
-        rules={[{ required: true }]}
+        rules={[
+          {
+            required: true,
+            whitespace: false,
+            message: "Input verification code",
+          },
+        ]}
         extra={
           <Button
             onClick={handleSendCode}
