584ca43a72b9d741b4aa0adede7f19fa66dd021c
tang <15977536060@163.com>
2023-06-26 14:52:14 +0800

    chore: according to the NODE_ENV to choose using proxy or not

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -3,8 +3,11 @@ import { Toast } from "antd-mobile";
 import axios from "axios";
 
 const instance = axios.create({
-  // baseURL: "http://localhost:8080",
+  baseURL:
+    process.env.NODE_ENV === "production" ? "http://api.tangzhenming.com" : "",
 });
+// 本地开发环境中直接向 http://api.tangzhenming.com 发送请求时，浏览器会阻止这个跨域请求。可以在本地开发环境中继续使用本地代理（setupProxy.js）来解决这个问题，而生产环境用 http://api.tangzhenming.com 发起请求
+// 在本地开发环境中使用 npm start 命令启动项目时，create-react-app 会自动将 process.env.NODE_ENV 的值设置为 "development"。当使用 npm run build 命令构建项目时，create-react-app 会自动将 process.env.NODE_ENV 的值设置为 "production"
 
 instance.interceptors.request.use((request) => {
   Toast.show({
