aea99544167e83045c78f87ae952e677f9282763
tang <15977536060@163.com>
2023-07-03 17:59:52 +0800

    refactor: Optimized router with a new component: RTTabBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,10 @@
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-import Login from "./pages/login/login";
-import Home from "./pages/home/home";
-import User from "./pages/user/user";
-import RequireAuth from "./router/RequireAuth";
+import RTTabBar from "./router/RTTabBar/RTTabBar";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Link to="/">home</Link> | <Link to="/login">login</Link> |{" "}
-        <Link to="/user">user</Link>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route
-            path="/user"
-            element={
-              <RequireAuth>
-                <User />
-              </RequireAuth>
-            }
-          />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <RTTabBar></RTTabBar>
+    </div>
   );
 }
 
---
+++ src/router/RTTabBar/RTTabBar.tsx
@@ -0,0 +1,59 @@
+import { TabBar } from "antd-mobile";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
+import { AppOutline, UserOutline } from "antd-mobile-icons";
+import Home from "../../pages/home/home";
+import "./RTTabBar.css";
+import User from "../../pages/user/user";
+import Login from "../../pages/login/login";
+import RequireAuth from "../RequireAuth";
+
+function RTTabBar() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { pathname } = location;
+
+  const setRouteActive = (value: string) => {
+    navigate(value);
+  };
+
+  const tabs = [
+    {
+      key: "/",
+      title: "首页",
+      icon: <AppOutline />,
+    },
+    {
+      key: "/user",
+      title: "我的",
+      icon: <UserOutline />,
+    },
+  ];
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/user"
+          element={
+            <RequireAuth>
+              <User />
+            </RequireAuth>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+
+      <TabBar
+        className="rt-tab-bar"
+        activeKey={pathname}
+        onChange={(value) => setRouteActive(value)}
+      >
+        {tabs.map((item) => (
+          <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+        ))}
+      </TabBar>
+    </>
+  );
+}
+
+export default RTTabBar;
