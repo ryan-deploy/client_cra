742e515751ba6fccc20e1a05dd2aa846d8a8e33b
tang <15977536060@163.com>
2023-07-04 18:34:36 +0800

    test: Test ReadPost API

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,3 +1,14 @@
-export default function Home() {
+import { useEffect } from "react";
+import { ReadPosts } from "../../api/post";
+
+const Home = () => {
+  useEffect(() => {
+    ReadPosts().then((res) => {
+      console.log(res);
+    });
+  }, []);
+
   return <div className="home">home</div>;
-}
+};
+
+export default Home;
