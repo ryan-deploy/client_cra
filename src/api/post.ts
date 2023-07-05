3d4a0c8e27fd02d67721719069115ded4898e469
tang <15977536060@163.com>
2023-07-05 10:25:23 +0800

    fix(important): Remove the last slash of ReadPosts, this cause a redirecting by gin when requesting the API

--- src/api/post.ts
+++ src/api/post.ts
@@ -1,6 +1,6 @@
 import axios from "./axios";
 
 export async function ReadPosts() {
-  const res = await axios.get("api/v1/posts/");
+  const res = await axios.get("api/v1/posts");
   return res.data;
 }
